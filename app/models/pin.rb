b1fb79e58ba62b93d70bb37fb7d3ec99c8cf4895
Kurtis Guy <kguy18@gmail.com>
2013-05-02 17:34:21 -0700

    Add Faker Gem

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -6,7 +6,7 @@ class Pin < ActiveRecord::Base
   validates :user_id, presence: true
   validates_attachment :image, presence: true,
   															content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] },
-  															size: { less_than: 5.megabytes }
+  															size: { less_than: 9.megabytes }
 
   belongs_to :user
   has_attached_file :image, styles: { medium: "320x240>", thumb: "160x120>" }
